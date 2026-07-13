0109af2df7a0990d3253d149d06334730bf7ef2f
mescon <mescon@localhost>
2026-07-13 20:44:01 +0200

    logi-dd: scaffold Rust workspace (core + tui crates)

---
+++ userspace/logi-dd/target/debug/deps/logi_dd_core-b93f1779b337f694.d
@@ -0,0 +1,5 @@
+/home/mescon/Projects/logitech-trueforce-linux-driver/userspace/logi-dd/target/debug/deps/logi_dd_core-b93f1779b337f694.d: crates/logi-dd-core/src/lib.rs
+
+/home/mescon/Projects/logitech-trueforce-linux-driver/userspace/logi-dd/target/debug/deps/logi_dd_core-b93f1779b337f694: crates/logi-dd-core/src/lib.rs
+
+crates/logi-dd-core/src/lib.rs:
---
+++ userspace/logi-dd/target/debug/deps/logi_dd_tui-9af7d8dffb9244ad.d
@@ -0,0 +1,5 @@
+/home/mescon/Projects/logitech-trueforce-linux-driver/userspace/logi-dd/target/debug/deps/logi_dd_tui-9af7d8dffb9244ad.d: crates/logi-dd-tui/src/main.rs
+
+/home/mescon/Projects/logitech-trueforce-linux-driver/userspace/logi-dd/target/debug/deps/logi_dd_tui-9af7d8dffb9244ad: crates/logi-dd-tui/src/main.rs
+
+crates/logi-dd-tui/src/main.rs:
